5e137a303c492022fa4e1e87a8c0b50aaf1e9ecb
vmusin <vmusin@bakapp.ru>
2019-03-11 10:41:44 +0300

    new commits to erachainds

--- src/main/java/org/erachain/controllers/TransactionController.java
+++ src/main/java/org/erachain/controllers/TransactionController.java
@@ -1,41 +1,22 @@
 package org.erachain.controllers;
 
-import org.erachain.entities.TransactionFactory;
-import org.erachain.entities.transactions.Transaction;
-import org.erachain.repositories.DBTransaction;
-import org.erachain.utils.loggers.LoggableController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-//@Controller
+import org.erachain.utils.crypto.Base58;
+import org.erachain.utils.loggers.LoggableController;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 @RequestMapping("/transaction")
 public class TransactionController {
 
-    @Autowired
-    private DBTransaction transProc;
-
-    @Autowired
-    private TransactionFactory transactionFactory;
-
     @LoggableController
-    @PostMapping(path = "/proc")
-    public ResponseEntity<String> createTransaction(@RequestParam("trans_token") String rawDataBase58) throws InterruptedException {
+    @RequestMapping(value = "/proc", method = RequestMethod.PUT, produces = "application/json")
+    public boolean createTransaction(@RequestParam("trans_token") String rawDataBase58) throws InterruptedException {
 
-        Transaction transaction  = transactionFactory.parse(rawDataBase58);
+        byte[] data = Base58.decode(rawDataBase58);
 
-        if (transaction == null){
-            return ResponseEntity.ok("{ \"error message\" : \"Invalid signature\"}");
-        }
-        else if (!transaction.isOk()) {
-            return ResponseEntity.ok("{ \"error message\": \"" + transaction.getErrMessage() + "\"}");
-        }
-
-        transProc.setTransaction(transaction);
-        return ResponseEntity.ok("ok done");
+        return true;
     }
 
 }
