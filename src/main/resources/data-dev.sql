1c76ae32e895ed6728fb0773bfc359a652abed53
gkolymagin <G.Kolymagin@ch-group.ru>
2019-10-08 14:38:11 +0300

    Удаление -dev.properties файлов

--- src/main/resources/data-dev.sql
+++
@@ -1,4 +0,0 @@
-INSERT OR IGNORE INTO Account (id, accountUrl , publicKey , privateKey, recipient , user, password, identityName) VALUES
-(1, 'https://app.yaenergetik.ru/api?v2','addLater', 'addLater','7LghJWXYwq8yJESBri4FbUcoiectMEw393','s.klokov@erachain.org', 'erachain', 'problem');
-
-
