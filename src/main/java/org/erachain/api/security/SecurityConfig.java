a283e72ceaa4137f3b4b72b6d7d2380a4e552b2c
gkolymagin <G.Kolymagin@ch-group.ru>
2019-10-14 16:47:02 +0300

    Авторизация. Исправлены файлы конфигурации. Авторизация работает. Добавлены скрипты.

--- src/main/java/org/erachain/api/security/SecurityConfig.java
+++ src/main/java/org/erachain/api/security/SecurityConfig.java
@@ -1,12 +1,17 @@
 package org.erachain.api.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 /**
  * Created by nydiarra on 06/05/17.
@@ -16,27 +21,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Autowired
-//    DataSource dataSource;
+    @Autowired
+    DataSource dataSource;
 
     @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("ADMIN_USER").password("adminpass").roles("ADMIN_ROLE");
-//        auth.jdbcAuthentication().dataSource(dataSource)
-//                .usersByUsernameQuery(
-//                        "SELECT user, password FROM Account WHERE user=?");
+//        auth.inMemoryAuthentication().withUser("s.klokov@erachain.org").password(passwordEncoder().encode("erachain")).authorities("ADMIN_ROLE");
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "SELECT Account.user as username, Account.password as paaword,'1' as enabled FROM Account WHERE user=?")
+                .authoritiesByUsernameQuery(
+                        "SELECT Account.user as username, 'admin_role' as role FROM Account WHERE user=?"
+                );
     }
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
                 .authorizeRequests()
-                .antMatchers("/api/v1/blockchain/**").hasRole("ADMIN_ROLE");
+                .antMatchers("/api/v1/blockchain/**").authenticated()
+                .and()
+                .httpBasic();
+    }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
