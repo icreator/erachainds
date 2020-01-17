5bf1a9b7652f9b5a65772721d8d2ccdb0b61c5a8
Pavel <p.sergeev@ch-group.ru>
2020-01-17 16:36:09 +0300

    Исправлены ощибки рассчета параметров даты с учетом часовых зон

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ https://app.swaggerhub.com/apis/Erachain/ErachainDS/1.0.0
 
 # Руководство администратора (по настройке БД) 
 
-https://ch-project.com/products/files/doceditor.aspx?fileid=502
+https://ch-project.com/products/files/doceditor.aspx?fileid=671
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,26 +11,28 @@ VALUES ('ErachainDS',
         '3tFgehzEnQFiamLXTZoqCHac4wgt1USSLgDyfwoTS7Cx1Fhg4deJ7qWCBb9rx9X4M6oVJGTkA7bvftwY795qfefG',
         '95ZmQZPPYxzziXi65iCzQ8pB85dYED5apWZGaxy8H3kR');
 
--- INSERT OR IGNORE INTO Account (accountUrl, accountRecipient, recipientPublicKey, user, password, identityName,
---                                objectName,
---                                idSender)
--- VALUES ('http://api-admin.testmoydom.ru',
---         '7Nv5UaDubU222yynbNQTsUCZUYr1qW6dx9',
---         'B9QCMXdDwyD98S2k1GSJMuxJPzvYBJ9Sr5fDPB1zYDV6',
---         's.klokov@erachain.org',
---         'erachain',
---         'problem',
---         'myDomService',
---         'ErachainDS');
---
--- INSERT OR IGNORE INTO Params (id, requestId, paramName, dataType, format, defValue, curValue)
--- VALUES (1,
---         1,
---         'authToken',
---         'char',
---         null,
---         '9u5bjskxv4pb7uotfk',
---         0);
+ INSERT OR IGNORE INTO Account (accountUrl, accountRecipient, recipientPublicKey, user, password, identityName,
+                                objectName,
+                                idSender,APIuser,APIpassword)
+ VALUES ('http://api-admin.testmoydom.ru',
+         '7Nv5UaDubU222yynbNQTsUCZUYr1qW6dx9',
+         'B9QCMXdDwyD98S2k1GSJMuxJPzvYBJ9Sr5fDPB1zYDV6',
+         's.klokov@erachain.org',
+         'erachain',
+         'problem',
+         'myDomService',
+         'ErachainDS',
+         'username',
+         'pass');
+
+ INSERT OR IGNORE INTO Params (id, requestId, paramName, dataType, format, defValue, curValue)
+ VALUES (1,
+         1,
+         'authToken',
+         'char',
+         null,
+         '9u5bjskxv4pb7uotfk',
+         0);
 
 -- INSERT OR IGNORE INTO Params (id, requestId, paramName, dataType, format, defValue, curValue)
 -- VALUES (2,
@@ -42,47 +44,47 @@ VALUES ('ErachainDS',
 --         1);
 
 
-INSERT OR IGNORE INTO Account (id, accountUrl, accountRecipient, recipientPublicKey, user, password, identityName,
-                               objectName,
-                               idSender,APIuser,APIpassword)
-VALUES (1,
-        'https://app.yaenergetik.ru/api?v2',
-        '7AjWLhrtBxxsw7zsoqT79FMU6VSY81NCr3',
-        'GA8XX9cm4W5ctsQLBcW6hu2LSYBSAT7mxSrsVWBDnJhw',
-        's.klokov@erachain.org',
-        'erachain',
-        'meter',
-        'energyService',
-        'ErachainDS',
-        'username',
-        'pass');
-
-INSERT OR IGNORE INTO Params (id, requestId, paramName, dataType, format, defValue, curValue)
-VALUES (1,
-        1,
-        'type',
-        'char',
-        '',
-        'hour',
-        0);
-
-INSERT OR IGNORE INTO Params (id, requestId, paramName, dataType, format, defValue, curValue)
-VALUES (2,
-        1,
-        'value',
-        'date',
-        'yyyy-MM-dd''T''HH:mm:ssZ',
-        '',
-        1);
-
-INSERT OR IGNORE INTO Params (id, requestId, paramName, dataType, format, defValue, curValue)
-VALUES (3,
-        1,
-        'mode',
-        '',
-        null,
-        'all',
-        null);
+--INSERT OR IGNORE INTO Account (id, accountUrl, accountRecipient, recipientPublicKey, user, password, identityName,
+--                               objectName,
+--                               idSender,APIuser,APIpassword)
+--VALUES (1,
+--        'https://app.yaenergetik.ru/api?v2',
+--        '7AjWLhrtBxxsw7zsoqT79FMU6VSY81NCr3',
+--        'GA8XX9cm4W5ctsQLBcW6hu2LSYBSAT7mxSrsVWBDnJhw',
+--        's.klokov@erachain.org',
+--        'erachain',
+--        'meter',
+--        'energyService',
+--        'ErachainDS',
+--        'username',
+--        'pass');
+--
+--INSERT OR IGNORE INTO Params (id, requestId, paramName, dataType, format, defValue, curValue)
+--VALUES (1,
+--        1,
+--        'type',
+--        'char',
+--        '',
+--        'hour',
+--        0);
+--
+--INSERT OR IGNORE INTO Params (id, requestId, paramName, dataType, format, defValue, curValue)
+--VALUES (2,
+--        1,
+--        'value',
+--        'date',
+--        'yyyy-MM-dd''T''HH:mm:ssZ',
+--        '',
+--        1);
+--
+--INSERT OR IGNORE INTO Params (id, requestId, paramName, dataType, format, defValue, curValue)
+--VALUES (3,
+--        1,
+--        'mode',
+--        '',
+--        null,
+--        'all',
+--        null);
 
 INSERT OR IGNORE INTO Request(id,
                               accountId,
@@ -93,8 +95,8 @@ INSERT OR IGNORE INTO Request(id,
                               timezone)
 VALUES (1,
         1,
-        'hour',
-        'day',
+        'minute',
+        'minute',
         'minute',
         1,
         'Europe/Moscow')
